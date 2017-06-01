2e7ea5455c84f14ff8776a21ba9af8417ab9e69e
Roj <joa.dev@live.com>
2017-06-01 01:44:49 -0300

    agrego columnas nuevas al merge; TODO: que sea en python

--- merge_incendios.R
+++ merge_incendios.R
@@ -1,6 +1,6 @@
 eventos <- read.csv("datos/focos_salida.csv")
 
-eventos <- eventos[,c("incendio_id","incendio_tam","incendio_inicio","incendio_fin","incendio_centro_lat","incendio_centro_lon")]
+eventos <- eventos[,c("incendio_id","incendio_tam","incendio_inicio","incendio_fin","incendio_centro_lat","incendio_centro_lon", "perimetro", "duracion", "velocidad")]
 
 eventos <- eventos[-which(duplicated(eventos$incendio_id)),]
 
