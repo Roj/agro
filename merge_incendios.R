dd44df75842b79d55769524c94529be572aed232
joaquintz <joa.dev@live.com>
2018-06-23 19:26:45 +0200

    Delete merge_incendios.R

--- merge_incendios.R
+++
@@ -1,8 +0,0 @@
-eventos <- read.csv("datos/focos_salida.csv")
-
-eventos <- eventos[,c("incendio_id","incendio_tam","incendio_inicio","incendio_fin","incendio_centro_lat","incendio_centro_lon", "perimetro", "duracion", "velocidad")]
-
-eventos <- eventos[-which(duplicated(eventos$incendio_id)),]
-
-
-write.csv(x=eventos,file="datos/focos_salida_agrupados.csv",row.names = FALSE)
